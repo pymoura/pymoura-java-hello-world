6e266435a69ad0d92ec5c1048c947da5f08aa925
Paula Yamashita <pymoura@gmail.com>
2022-02-19 15:05:07 -0800

    improve tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,17 +1,8 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-<<<<<<< HEAD:src/test/java/HelloWorldTest.java
-public class HelloWorldTest {
-    private HelloWorld helloWorld;
 
-    @BeforeEach
-    public void init(){
-        helloWorld = new HelloWorld();
-    }
-=======
-public class MainTest {
->>>>>>> 3d54193a7e185e54c19bae236615d9a21dac93b4:src/test/java/MainTest.java
+public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
